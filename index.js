2a286cec0386179610611c36d32b05a7ed27a1ac
Fortune <Fortune971114@protonmail.com>
2023-04-25 14:07:35 +0200

    THE FOR LOOP WAS FINALLY FIXED DAMN. YOU BY FORTUNE THE MANNIS

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ console.log("Git Hello");
 
 let l = ["Python", "C", "Java", "JavaScript"];
 
+console.log("Programming Languages");
+
 for (let i = 0; i < 4; i++) {
-	console.log(l(i));
+	console.log(l[i]);
 };
